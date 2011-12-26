9c2b37005ae62370bea96636cc56e981ad6fbe14
rodeyseijkens <me@rodey.nl>
2011-12-26 04:07:33 +0100

    Forgot the search translation...

--- lib/siriproxy-eyelp.rb
+++ lib/siriproxy-eyelp.rb
@@ -114,7 +114,7 @@ class SiriProxy::Plugin::Eyelp < SiriProxy::Plugin
     	$mapla = object["properties"]["latitude"]
 	end 
 
-listen_for /suche (.*)/i do |phrase|
+listen_for /search (.*)/i do |phrase|
 	ss = ""
 	#if phrase.match(/(ein )/)  # cleaning searchstring: eg:  ein hotel = hotel
 	phrase = phrase.insert(0, " ")
